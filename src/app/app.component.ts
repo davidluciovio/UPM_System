83df808bfddfd3f97e8ae7087a49582c80037d9e
Luis David Work <davidluciovio@gmail.com>
2025-04-09 14:57:02 -0600

    Initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { PrimeNG } from 'primeng/config';
+import { ButtonModule } from 'primeng/button';
+import { SelectModule } from 'primeng/select';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    SelectModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private primeng: PrimeNG) {}
+
+  ngOnInit(): void {
+    this.primeng.ripple.set(true);
+  }
+
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    element!.classList.toggle('dark-mode');
+}
   title = 'upmSystemWeb';
 }
